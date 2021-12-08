1639a3c751dd5c86205badd8449b0891fc6d6631
石力 <shili@miemie.la>
2021-12-08 09:17:02 +0800

    :feet: :feet:

--- main.go
+++ main.go
@@ -7,14 +7,23 @@ import (
 
 func main() {
 	r := gow.New()
+	r.GET("/", func(c *gow.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+
 	r.GET("/hello", func(c *gow.Context) {
+		// expect /hello?name=geektutu
 		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
-	r.POST("/", func(c *gow.Context) {
-		c.JSON(http.StatusOK, gow.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
-		})
+
+	r.GET("/hello/:name", func(c *gow.Context) {
+		// expect /hello/geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
 	})
-	r.Run(":9001")
+
+	r.GET("/assets/*filepath", func(c *gow.Context) {
+		c.JSON(http.StatusOK, gow.H{"filepath": c.Param("filepath")})
+	})
+
+	r.Run(":9999")
 }
